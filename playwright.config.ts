a22979ea0867dc68e3d1cc1ca1860ab3dd767640
Radu Brehar <radu@jslog.com>
2024-03-20 19:18:17 +0200

    add watch script

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./src/pages",
+  timeout: process.env.CI ? 10000 : 4000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
